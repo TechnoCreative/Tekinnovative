9adf9950da7f3908e3321082dbfe3a2ee5b93348
EC2 Default User <ec2-user@ip-172-31-94-144.ec2.internal>
2019-09-15 19:46:35 +0000

    First line of NewsletterController removed

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,5 +1,5 @@
 class NewslettersController < ApplicationController
-    skip_before_action :authenticate_user!
+    # skip_before_action :authenticate_user!
     before_action :set_newsletter, only: [:destroy]
     
     def îndex 
