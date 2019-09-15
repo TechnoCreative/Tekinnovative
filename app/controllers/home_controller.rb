12168ee8190a1309566fff2629d9e08c41fab40b
EC2 Default User <ec2-user@ip-172-31-94-144.ec2.internal>
2019-09-15 19:50:21 +0000

    index of HomeController updated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     
     def index 
         
-        # @subscriber = Newsletter.create
+        @subscriber = Newsletter.create
         
         
        
